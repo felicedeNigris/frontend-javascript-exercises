8aab8787dbe5e63da1762db6c8161c8378b0bf44
Felice DeNigris <felideni@gmail.com>
2015-03-18 01:10:59 +0000

    Created functions that pass on mocha

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,9 +1,20 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  if(stringOne === stringTwo){
+    return true;
+  {
+  else
+  {
+    return false;
+  }
 };
 
 module.exports.notEqual = function(one, two) {
-
+  if(one !== two && one !=== two){
+    return true;
+  }
+  else
+  {
+    return false;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
