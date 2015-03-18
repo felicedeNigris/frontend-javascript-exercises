3ef4544868e8717c6f5d825596bede012e776f0b
Felice DeNigris <felideni@gmail.com>
2015-03-18 02:31:48 +0000

    functions newArray & firstAndLast passing

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,21 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a,b,c,d){
+var arrayArgs = [];
+for(i =0; i< arguments.length; i++){
+   arrayArgs.push(arguments[i]); 
+}
 
-module.exports.firstAndLast = undefined;
+return arrayArgs;
+
+};
+
+module.exports.firstAndLast = function(arrayitem){
+  var pushArray =[];
+  if(arrayitem instanceof Array && arrayitem.length > 2){
+    pushArray.push(arrayitem[0]);
+    pushArray.push(arrayitem.pop());
+    return pushArray;
+  }
+  else{
+    return null;
+  }
+};
