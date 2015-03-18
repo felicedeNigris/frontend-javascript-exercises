6807b0aeeb8e62fae22075a4e8c7ea176600cc49
Felice DeNigris <felideni@gmail.com>
2015-03-18 04:12:57 +0000

    Array exercises 1 & 2

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,52 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(arry){
+    var returnArray = [];
+    if(arry instanceof Array && arry.length > 1){
+        for(i =0; i< arry.length; i++){
+          returnArray.push(arry[i]);
+        }
+        returnArray = returnArray.reverse();
+        returnArray.unshift(1);
+        return returnArray;
+    }
+    else
+    {
+        return null;
+    }
 
-module.exports.plusesEverywhere = undefined;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.plusesEverywhere = function(arry){
+
+var str = "";
+    if(arry instanceof Array && arry.length > 0){
+      for(b =0; b < arry.length; b++){
+          str += arry[b] + "+";
+      }
+      return str.slice(0,-1);
+    }
+    else
+    {
+      return null;
+    }
+
+};
+
+module.exports.arrayQuantityPlusOne = function(arry){
+
+  checkifNumber = function(element, index, array){
+     if((typeof element) == 'number'){
+        return true;
+    }
+    else{
+        return false;
+    }
+  };
+
+  if(arry instanceof Array && arry.every(checkifNumber)){
+    return arry.length + 1;
+  }
+  else
+  {
+    return null;
+  }
+};
