896e1dfd74191d825acef32e14300b56429ae528
Felice DeNigris <felideni@gmail.com>
2015-03-18 04:49:28 +0000

    Added last Array exercise

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,24 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(arg, list){
+    for(i=0;i<list.length; i++){
+        if((list[i] === arg) === true){
+           return list;//list.splice(list[i],1);
+        }
+        else
+        {
+           list.push(arg);
+           return list;
+        }
+    }
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(arry){
+  if(arry instanceof Array){
+    return arry.reverse(arry.sort());
+  }
+  else{
+    return arry;
+  }
+
+  
+
+};
