55e7177af44265d739977480e37d6fd57a474d17
Felice DeNigris <felideni@gmail.com>
2015-03-19 04:08:17 +0000

    Boolean testing functions passing to mocha

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,7 +1,7 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
   if(stringOne === stringTwo){
     return true;
-  {
+  }
   else
   {
     return false;
@@ -9,22 +9,44 @@ module.exports.equalStrings = function(stringOne, stringTwo) {
 };
 
 module.exports.notEqual = function(one, two) {
-  if(one !== two && one !=== two){
+  if((one !== two) || (one != two)){
     return true;
   }
   else
   {
     return false;
+  }
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+    if(parseInt(middle, 10) > parseInt(lower,10) && parseInt(middle,10) < parseInt(upper,10)){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 };
+
 
 module.exports.outsideRanges = function(number) {
-
+    if((typeof number == 'number') && !(number <=20 && number >= 10) && !(number > 42 && number <= 75) && !(number > 1 &&  number < 6)){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 };
 
-module.exports.nameAndPrice = function(name, price) {
 
+
+module.exports.nameAndPrice = function(name, price) {
+    if((name == "NYTimes" || name == "LATimes") && price >= 1){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 };
