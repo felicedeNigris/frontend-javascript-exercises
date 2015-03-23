11d8bc9331798278c81e2e56d69fd6574c7f85c0
Felice DeNigris <felideni@gmail.com>
2015-03-23 23:58:22 +0000

    all loops and arrays passing

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,28 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(sampleArray){
+    var arraySum = 0;
+    if(sampleArray.length === 0){
+        return 0;
+    }
+    else
+    {
+      for(i = 0; i < sampleArray.length; i++){
+        arraySum += sampleArray[i];
+    }
+    return arraySum;
+    }
+};
 
-module.exports.splitAndLowerCaseString = undefined;
 
-module.exports.addIndex = undefined;
+module.exports.splitAndLowerCaseString = function(inputString){
+  return inputString.toLowerCase().split(',');   
+
+};
+
+module.exports.addIndex = function(sampleArray){
+    var newarray =[];
+    for(i =0; i< sampleArray.length; i++){
+        newarray.push(i + " is " + sampleArray[i]);
+    }
+    return newarray;
+
+};
