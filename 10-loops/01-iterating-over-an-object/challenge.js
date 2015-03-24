963f07b358f25c593c43639f013bba3a6619a17f
Felice DeNigris <felideni@gmail.com>
2015-03-24 00:37:54 +0000

    Passed 10-loops/01-iterating-over-an-object

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,24 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(argObj){
+    objKeys = [];
+    for(var i in argObj){
+      objKeys.push(i);
+    }
+    return objKeys;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(argObj){
+    objKeys = [];
+    for(var i in argObj){
+      objKeys.push(argObj[i]);
+    }
+    return objKeys;
+};
 
-module.exports.objectToArray = undefined;
+
+module.exports.objectToArray= function(argObj){
+    objKeyValue = [];
+    for(var i in argObj){
+      objKeyValue.push( i +" is " + argObj[i]);
+    }
+    return objKeyValue;
+};
