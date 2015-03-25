2d8bc06e4bbc8d3fcf2be43c29898b698b3e644d
Felice DeNigris <felideni@gmail.com>
2015-03-25 00:01:49 +0000

    Looping lesson 10 passing

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,36 @@
-module.exports.copy = undefined;
+module.exports.copy = function(argObj){
+    var newObj ={};
+    for(var i in argObj){
+        newObj[i] = argObj[i];
+    }
+    return newObj;
+};;
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest,src){
+    for(var i in src){
+        if((i in dest) === true){
+          dest[i] = src[i];
+       }
+       else if((i in dest)===false){
+           dest[i] = src[i];
+       }
+    }
+    return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(o,a){
+  var oarray = Object.keys(o).sort(); //sorted object properties
+  var a2 = a.sort(); //sorted array
+    for(var x =0; x <= a.length; x ++){
+        if(oarray[x] === a2[x] || a.length === 0){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+
+  
