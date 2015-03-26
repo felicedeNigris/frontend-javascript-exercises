d45f61a3e796a69dff700b65e2706e5890e9a344
Felice DeNigris <felideni@gmail.com>
2015-03-26 23:48:31 +0000

    Loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,21 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn,actionFn){
+    while( conditionalFn() ){ //conditional function returns a boolean (while loop is in conditionalFn)
+        
+        actionFn(); //call action function
+    }
+};
 
-module.exports.sumNumbers = undefined;
+
+module.exports.sumNumbers =   function(argarr){
+    counter = argarr.length;
+    var totalsum = 0;
+    while(counter >= 1){
+        counter--;
+        console.log(argarr[counter]);
+        totalsum += argarr[counter];
+        
+    }
+    return totalsum;
+};
+;
 
