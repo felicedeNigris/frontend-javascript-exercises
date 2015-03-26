26ba316ef60ffeac3b7b648e9a5398717295edf5
Felice DeNigris <felideni@gmail.com>
2015-03-26 23:53:12 +0000

    Loops 2.0

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -9,11 +9,11 @@ module.exports.stream = function(conditionalFn,actionFn){
 module.exports.sumNumbers =   function(argarr){
     counter = argarr.length;
     var totalsum = 0;
-    while(counter >= 1){
-        counter--;
-        console.log(argarr[counter]);
-        totalsum += argarr[counter];
-        
+    while(counter >= 1){ // while greater than one
+        counter--; 
+        console.log(argarr[counter]);//print array element[counter]
+
+        totalsum += argarr[counter];// add the sum to totalsum variable
     }
     return totalsum;
 };
